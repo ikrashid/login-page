1a1772f009ab9ea096c8e6bba05815fc5bb29159
Ikra Rashid <ikra.rashid252@gmail.com>
2021-05-30 12:16:38 -0400

    using useEffect to check form validity and using cleanup function to check after 6 seconds of no change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,25 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(()=>{
+    // app is reloaded, react goes thru entire component
+    // anon function is run because page reloaded so dependencies are considered to have changed 
+    // then, state changes and app is rerendered. react goes thru entire code, updating dom if need be, then asses useEffect. dependencies havent changed, so anon function isnt rerun
+    const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn');
+    if (storedUserLoggedInInfo === '1'){
+      setIsLoggedIn(true);
+    }
+  }, [])
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
