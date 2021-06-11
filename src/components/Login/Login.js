3f0709777e3984f48b5516883d8f17c0f212d35c
Ikra Rashid <ikra.rashid252@gmail.com>
2021-06-11 06:35:09 -0400

    created custom context provider component to handle logging in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   // return {
@@ -67,6 +68,8 @@ const Login = (props) => {
     isValid: null
   })
 
+  const authCtx = useContext(AuthContext);
+
   useEffect(()=>{
     // using useEffect clean up function so we check form validity after user hasnt typed for a certain amount of seconds
     const identifier = setTimeout(()=>{
@@ -116,7 +119,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
